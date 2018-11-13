d2b84073db910d0cbc8245ea02838509c79dd036
Maksym_Oleshchuk <Maksym_Oleshchuk@epam.com>
2018-11-13 15:52:05 +0200

    Prettify code

--- src/main/java/com/epam/task/SubTask.java
+++ src/main/java/com/epam/task/SubTask.java
@@ -3,20 +3,20 @@ package com.epam.task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SumTask implements Task {
+public class SubTask implements Task {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SumTask.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SubTask.class);
 
     private final int a;
     private final int b;
 
-    public SumTask(int a, int b) {
+    public SubTask(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
     @Override
     public void process() {
-        LOGGER.info("Sum of {} and {} is {}", a, b , a+b);
+        LOGGER.info("Sum of {} and {} is {}", a, b , a-b);
     }
 }
